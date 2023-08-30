666389962b9c467fab516bb0cf5d58d53deed755
EnzoDall <94904990+EnzoDall@users.noreply.github.com>
2023-08-29 22:37:59 -0300

    Update Lista.js

--- src/components/Lista.js
+++ src/components/Lista.js
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import './Lista.css'; // Importe o arquivo de estilo do componente
-
+import './Lista.css';
 function Lista() {
   const [recipes, setRecipes] = useState([]);
 
